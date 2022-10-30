72a5b8498c59c630198bf55c22dd61de0f1e74fb
unknown <zcaptrm@ucl.ac.uk>
2022-10-30 20:57:18 +0000

    updated code

--- datascript.R
+++ datascript.R
@@ -1,1 +1,21 @@
-test
+library(dplyr)
+library(here)
+library(sf)
+library(tmap)
+library(countrycode)
+
+data <- read.csv(here::here(('HDR21-22_Composite_indices_complete_time_series.csv'))) %>%
+  dplyr::select(starts_with("hdi_2010"), starts_with("hdi_2019"), contains("country")) %>%
+  mutate(hdi_diff = hdi_2019 - hdi_2010)
+
+shapefile <- st_read(here::here('World_Countries_(Generalized)','World_Countries__Generalized_.shp'))
+
+plotdata <- shapefile %>%
+  merge(., 
+        data,
+        by.x = 'COUNTRY',
+        by.y = 'country')
+tmap_mode('plot')
+
+plotdata %>%
+  qtm(.,fill = 'hdi_diff')
